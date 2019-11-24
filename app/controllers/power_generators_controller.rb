be77386dd7acd83e1d15f950e9700aafc88e5a3e
Karina Formentini Dinella <kaformentini@gmail.com>
2019-11-24 11:46:48 -0300

    Search works, find in name and description

--- app/controllers/power_generators_controller.rb
+++ app/controllers/power_generators_controller.rb
@@ -8,17 +8,24 @@ class PowerGeneratorsController < ApplicationController
   end
 
   def search
-    @power_generators = PowerGenerator.where("description LIKE ?", "%#{params[:search_power_generator]}%")
-  
-    # if @power_generators.empty?
-    #   flash.now[:error] = "Não há nenhuma receita com esse nome"
-    # end
+    @power_generators = PowerGenerator.where(
+      "name LIKE :param or description LIKE :param", 
+      {param: "%#{params[:search_power_generator]}%"}
+    )
+    
+    if @power_generators.empty?
+      flash.now[:error] = 'Descupe! não encontramos nenhum produto'
+    else
+      flash.now[:notice] = 'Encontramos esse(s) produto(s)'
+
+    end
+    
   end
 
   private
-  def power_enerator_params
-    params.require(:power_generators).permit(:name, :description, :image_url, :manufacturer, :structure_type, 
-                                            :price, :height, :width, :lenght, :weight, :kwp)
-  end
+  # def power_enerator_params
+  #   params.require(:power_generators).permit(:name, :description, :image_url, :manufacturer, :structure_type, 
+  #                                           :price, :height, :width, :lenght, :weight, :kwp)
+  # end
 
 end
--- spec/features/user_search_spec.rb
+++ spec/features/user_search_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 feature 'User search for a product' do
-  scenario 'successfully' do
-    product_110v = create(:power_generator, name:'Produto 1', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
+  scenario 'successfully find by description' do
+    product_110v = create(:power_generator, name:'PRODUTO 1', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
                                                                             Monitoramento inteligente de 4 strings e 80% de economia de tempo na detecção de falhas    
                                                                             Portas RS485 e USB para conectividade e gerenciamento de dados    
                                                                             Display LCD gráfico local e monitoramento remoto. Voltagem 110V" )
-    product_220v = create(:power_generator, name:'Produto 2', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
+    product_220v = create(:power_generator, name:'PRODUTO 2', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
                                                                             Monitoramento inteligente de 4 strings e 80% de economia de tempo na detecção de falhas    
                                                                             Portas RS485 e USB para conectividade e gerenciamento de dados    
                                                                             Display LCD gráfico local e monitoramento remoto. Voltagem 220V" )
@@ -17,8 +17,49 @@ feature 'User search for a product' do
     click_on "Pesquisar"
 
     expect(current_path).to eq(search_power_generators_path)
-    expect(page).to have_content('Encontramos esses produtos')
+    expect(page).to have_content('Encontramos esse(s) produto(s)')
     expect(page).to have_content(product_220v.name)
     expect(page).not_to have_content(product_110v.name)
   end
+  
+  scenario 'successfully find by name' do
+    product_110v = create(:power_generator, name:'PRODUTO 1', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
+                                                                            Monitoramento inteligente de 4 strings e 80% de economia de tempo na detecção de falhas    
+                                                                            Portas RS485 e USB para conectividade e gerenciamento de dados    
+                                                                            Display LCD gráfico local e monitoramento remoto. Voltagem 110V" )
+    product_220v = create(:power_generator, name:'PRODUTO 2', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
+                                                                            Monitoramento inteligente de 4 strings e 80% de economia de tempo na detecção de falhas    
+                                                                            Portas RS485 e USB para conectividade e gerenciamento de dados    
+                                                                            Display LCD gráfico local e monitoramento remoto. Voltagem 220V" )
+
+
+    visit root_path
+    fill_in "o que procura?", with: 'PRODUTO 2'
+    click_on "Pesquisar"
+
+    expect(current_path).to eq(search_power_generators_path)
+    expect(page).to have_content('Encontramos esse(s) produto(s)')
+    expect(page).to have_content(product_220v.name)
+    expect(page).not_to have_content(product_110v.name)
+  end
+
+  scenario 'user do not find any product' do
+    product_110v = create(:power_generator, name:'PRODUTO 1', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
+                                                                            Monitoramento inteligente de 4 strings e 80% de economia de tempo na detecção de falhas    
+                                                                            Portas RS485 e USB para conectividade e gerenciamento de dados    
+                                                                            Display LCD gráfico local e monitoramento remoto. Voltagem 110V" )
+    product_220v = create(:power_generator, name:'PRODUTO 2', description: "2 MPPTs para adaptação versátil a diferentes tipos ou quantidades de módulos construídos com diferentes alinhamentos    
+                                                                            Monitoramento inteligente de 4 strings e 80% de economia de tempo na detecção de falhas    
+                                                                            Portas RS485 e USB para conectividade e gerenciamento de dados    
+                                                                            Display LCD gráfico local e monitoramento remoto. Voltagem 220V" )
+
+
+    visit root_path
+    fill_in "o que procura?", with: '330V'
+    click_on "Pesquisar"
+
+    expect(current_path).to eq(search_power_generators_path)
+    expect(page).to have_content('Descupe! não encontramos nenhum produto')
+  end
+
 end
